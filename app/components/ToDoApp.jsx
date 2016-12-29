a770dad046c929cf9d388b1f9c616dd918d91979
shadsluiter <shadsluiter@shads-MacBook-Air.local>
2016-12-28 19:33:58 -0700

    static list rendered

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,12 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
-
+getInitialState: function() {
+  return {
+    todos: [
+      {
+        id: 1,
+        text: 'Walk the dog'
+      },
+      {
+        id: 2,
+        text: 'Clean the yard'
+      },
+      {
+        id: 3,
+        text: 'Learn React'
+      },
+      {
+        id: 4,
+        text: 'Get smarter'
+      },
+    ]
+  };
+},
 render: function() {
+  var {todos} = this.state;
   return (
-
     <div>
-      ToDoApp.jsx
+      <TodoList todos= {todos} />
     </div>
   )
 
